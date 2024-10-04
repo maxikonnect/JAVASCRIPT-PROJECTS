8c3dd5f5f2e08ac42b5934d10ccb95f19262f8ee
Abradu Frimpong Kwame <133316288+maxikonnect@users.noreply.github.com>
2024-10-04 15:24:13 +0000

    Add files via upload

--- BACKGROUNDCOLOR CHANGER/script.js
+++ BACKGROUNDCOLOR CHANGER/script.js
@@ -1,19 +1,43 @@
-// Select the paragraph element for color display
-const nameOfColor = document.querySelector(".nameOfColor");
+//We are going to access our paragraph and button using our created classes
+const colorName = document.querySelector(".colorName");
+const changeColor = document.querySelector(".changeColor");
+const SaveColor = document.querySelector(".SaveColor");
+const savedColors = document.querySelector(".savedColors");
 
-// Select the button element for changing color
-const colorChanger = document.querySelector(".colorChanger");
 
-// Add event listener to the button
-colorChanger.addEventListener("click", function() {
-    // Generate three random numbers for RGB color
+/*we now add an eventListener to our button using its class and add a function that we would call
+changeMyColor
+*/
+
+changeColor.addEventListener("click", changeMyColor);
+
+
+//We start to create our function
+function changeMyColor(){
+    //we create three random numbers using Math.floor and Math.random();
+    //Math.floor rounds a number to its lowest whole number let see it
+    //we comment it out 
+    //console.log(Math.floor(5.4)) //Gives us 5
+    //Math.random(), Gives us a randomnumber between 0 and 1. 1 is not included though
+    //console.log(Math.random());
+    //LET US NOW CREATE OUR RANDOM NUMBERS
     let randomNumber1 = Math.floor(Math.random() * 256);
     let randomNumber2 = Math.floor(Math.random() * 256);
     let randomNumber3 = Math.floor(Math.random() * 256);
-
-    // Update the paragraph text with the new color value
-    nameOfColor.textContent = `BackgroundColor: rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+    //This generates random numbers between 0 and 255 inclusive
+    //Next we input our colors using rgb
+    //Before that let us store rgb in a variable;
+    let color = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+    colorName.textContent = `BackgroundColor: ${color}`;
     
-    // Change the background color of the body
-    document.body.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
-});
+    //We now change color of window using document.body
+    document.body.style.backgroundColor = color;
+    //Our code is working now. LET US ADD OUR CSS NOW
+}
+
+
+//LET US LOG OUR CODE TO SEE IF IT WORKS
+console.log(colorName);
+console.log(changeColor);
+
+//press F12 on your PC/mac to see the consolein your browser.
